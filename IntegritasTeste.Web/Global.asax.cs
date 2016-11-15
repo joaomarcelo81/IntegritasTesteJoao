9a0f0cf2c72359754b3424847d8f3dcb7dcd640b
joaomarcelo81 <joaomarcelo@msn.com>
2016-11-14 22:42:32 -0200

    teste

--- IntegritasTeste.Web/Global.asax.cs
+++ IntegritasTeste.Web/Global.asax.cs
@@ -13,6 +13,8 @@ namespace IntegritasTeste.Web
     {
         protected void Application_Start()
         {
+
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
